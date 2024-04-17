a868f2ea775b83233d07013e1126767cca238506
Mottais <mottais.afy@outlook.fr>
2024-04-17 17:42:24 +0200

    Correction pycostyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""init api"""
+"""import thing here to avoid circular import"""
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+if True:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
