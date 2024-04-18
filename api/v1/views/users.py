7f53d1f450b41d9ac93d311467f40a4f8978088b
Mottais <mottais.afy@outlook.fr>
2024-04-18 23:39:51 +0200

    Gestion "Not a JSON"

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -42,22 +42,26 @@ def del_user(user_id):
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """creates a new user"""
-    req = request.get_json()
 
-    if req is None:
-        abort(400, 'Not a JSON')
-    if 'email' not in req:
-        abort(400, 'Missing email')
-    if 'password' not in req:
-        abort(400, 'Missing password')
+    try:
+        req = request.get_json()
+        if 'email' not in req:
+            abort(400, 'Missing email')
+        if 'password' not in req:
+            abort(400, 'Missing password')
 
-    email = req['email']
-    password = req['password']
+        email = req['email']
+        password = req['password']
 
-    new_user = User(email=email, password=password)
-    new_user.save()
+        new_user = User(email=email, password=password)
+        new_user.save()
 
-    return jsonify(new_user.to_dict()), 201
+        return jsonify(new_user.to_dict()), 201
+
+    except Exception:
+        response = jsonify({"message": "Not a JSON"})
+        response.status_code = 400
+        return response
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
@@ -67,14 +71,18 @@ def update_user(user_id):
     if not user:
         abort(404)
 
-    req = request.get_json()
-    if req is None:
-        abort(400, 'Not a JSON')
+    try:
+        req = request.get_json()
 
-    for key, value in req.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(user, key, value)
+        for key, value in req.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(user, key, value)
 
-    user.save()
+        user.save()
 
-    return jsonify(user.to_dict()), 200
+        return jsonify(user.to_dict()), 200
+
+    except Exception:
+        response = jsonify({"message": "Not a JSON"})
+        response.status_code = 400
+        return response
